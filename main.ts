776031008377a94fb732d055f1fe9b8184dd9e65
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-04-09 22:49:09 -0500

    html parser

--- main.ts
+++ main.ts
@@ -1,61 +1,139 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, request, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	request,
+	Setting,
+} from "obsidian";
+// import { parse, default as HTMLElement } from "node-html-parser/dist/nodes/html.js";
+import parse, { HTMLElement } from "node-html-parser";
 
 interface StackOverflowAnswerSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: StackOverflowAnswerSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class StackOverflowAnswers extends Plugin {
 	settings: StackOverflowAnswerSettings;
+
+	extractAnswerId(url: string) {
+		return url.split("#").pop();
+	}
+
+	extractParagraphs(html: HTMLElement) {
+		const paragraphs: string [] = []
+		
+		html.querySelectorAll("p").forEach((p) => {
+			paragraphs.push(p.innerHTML);
+		});
+
+		return paragraphs;
+	}
+
+	extractCodeBlocks(html: HTMLElement) {
+		const codeBlocks: string [] = []
+
+		html.querySelectorAll("pre").forEach((pre) => {
+			console.log(pre.querySelectorAll("code"));
+			codeBlocks.push(pre.innerHTML);
+		});
+
+		return codeBlocks;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Sample Plugin",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice("This is a notice!");
+			}
+		);
 
-		const res = await request({url: 'https://stackoverflow.com/questions/14122919/how-to-hide-show-objects-using-three-js-release-54/14123978#14123978'})
-		console.log(res);
+
+		const url = "https://stackoverflow.com/questions/14122919/how-to-hide-show-objects-using-three-js-release-54/14123978#14123978"
+
+		const res = await request({
+			url: url,
+		});
+		const root = parse(res, {
+			lowerCaseTagName: false, // convert tag name to lower case (hurts performance heavily)
+			comment: false, // retrieve comments (hurts performance slightly)
+			blockTextElements: {
+				script: false, // keep text content when parsing
+				noscript: true, // keep text content when parsing
+				style: false, // keep text content when parsing
+				pre: true, // keep text content when parsing
+			},
+		});
+		const struct = root.getElementById(`answer-${this.extractAnswerId(url)}`).querySelector('div.js-post-body');
+		console.log(this.extractParagraphs(struct));
+		console.log(this.extractCodeBlocks(struct));
+		
+		/*
+		<p>Is the 'mesh' variable you are sending the 'traverseHierarchy' function an Object3d?
+		If so have you tried 'mesh.children' which should return an array of child objects, or you could use the traverse function on the mesh object.</p>
+
+		<p>See: <a href="http://mrdoob.github.com/three.js/docs/54/#Reference/Core/Object3D" rel="noreferrer">http://mrdoob.github.com/three.js/docs/54/#Reference/Core/Object3D</a></p>
+
+		<pre><code>mesh.traverse(function(child) {
+			var z = document.getElementById("cameras").selectedIndex * 5 - 10;
+			if (z === -10) {
+				child.visible = true;
+			} else if (child.position.z !== z) {
+				child.visible = false;
+			} else {
+				child.visible = true;
+			};
+		});
+		</code></pre>
+		*/
+
+
 
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-sample-modal-simple",
+			name: "Open sample modal (simple)",
 			callback: () => {
 				new SampleModal(this.app).open();
-			}
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -66,7 +144,7 @@ export default class StackOverflowAnswers extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -74,20 +152,24 @@ export default class StackOverflowAnswers extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -101,12 +183,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -120,22 +202,24 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
